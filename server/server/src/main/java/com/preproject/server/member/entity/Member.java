ba87670bde83c4dab7539b5566aace0fb2f3854e
JanuaryKim <unool6976@gmail.com>
2022-10-25 16:12:08 +0900

    feat:멤버컬럼 수정

--- server/server/src/main/java/com/preproject/server/member/entity/Member.java
+++ server/server/src/main/java/com/preproject/server/member/entity/Member.java
@@ -7,10 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,13 +20,17 @@ public class Member extends Auditable {
     @Id
     private Long memberId;
 
+
     @Length(min = 16, max = 16)
+    @Column(nullable = false)
     private String memberPwd;
 
     @Length(min = 1, max = 50)
+    @Column(nullable = false)
     private String memberEmail;
 
     @Length(min = 16, max = 16)
+    @Column(nullable = false)
     private String memberName;
 
     @Length(max = 300)

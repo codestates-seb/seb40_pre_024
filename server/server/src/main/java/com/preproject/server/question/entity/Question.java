7f70a47a9f200a391257a3f345168e777c1af4ce
Youngseoki <dudtjrdl1492@gmail.com>
2022-10-26 11:30:41 +0900

    feat : question 연관관계 매핑

--- server/server/src/main/java/com/preproject/server/question/entity/Question.java
+++ server/server/src/main/java/com/preproject/server/question/entity/Question.java
@@ -1,12 +1,16 @@
 package com.preproject.server.question.entity;
 
+import com.preproject.server.answer.entity.Answer;
 import com.preproject.server.auditable.Auditable;
+import com.preproject.server.member.entity.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -19,16 +23,23 @@ public class Question extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long questionId;
 
-    @Length(min = 5, max = 100)
-    @Column(nullable = false) // length = 100 글자수 제한
+    @Column(nullable = false)
     private String questionTitle;
 
-    @Length(min = 15, max = 1000)
     @Column(nullable = false)
     private String questionContent;
 
     @Column(nullable = false)
     private int questionViewed;
 
-//    private int questionVote;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    public void addMember(Member member) {
+        this.member = member;
+    }
+
+    @OneToMany(mappedBy = "answer")
+    private List<Answer> answer = new ArrayList<>();
 }

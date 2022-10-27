5c7b7fcb1640fa6d67fda6d06341971f559586ac
yoosmg <sryu2s@gmail.com>
2022-10-27 21:15:34 +0900

    feat : 입력값 관리 방식 변경

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
+import React, { forwardRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
-export default function TextEditor() {
-  const [text, setText] = useState('');
-  const editorRef = React.createRef();
-  const onChange = () => {
-    const data = editorRef.current.getInstance().getHTML(); // getHTML or getMarkdown
-    setText(data);
-    console.log(text);
-  };
+const TextEditor = forwardRef((props, ref) => {
   return (
     <>
       <Editor
-        initialValue="<strong>Testing</strong>" // 초기 입력값
-        previewStyle="vertical" // vertical로 설정시 미리보기 화면 분할가능
+        initialValue="Please type here..." // 초기 입력값 설정
+        previewStyle="tab" // vertical로 설정시 미리보기 화면 분할가능
         height="300px"
         initialEditType="markdown" // markdown or wysiwyg
-        ref={editorRef}
-        onChange={onChange}
+        ref={ref}
+        onChange={props.onChange} // 부모 컴포넌트에서 ref, props 설정
+        // (e.g. <TextEditor ref={editorRef} onChange={getHTML}/>
       />
     </>
   );
-}
+});
+
+TextEditor.displayName = 'TextEditor';
+
+export default TextEditor;

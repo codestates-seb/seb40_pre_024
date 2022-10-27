c5ac9e33fcfbacf628aebd4869ee65bc8f1af623
dongwon <ehddnjs9672@gmail.com>
2022-10-27 12:33:57 +0900

    feat: Answer controller, mapper, repository, service 추가

--- server/server/src/main/java/com/preproject/server/exception/ExceptionCode.java
+++ server/server/src/main/java/com/preproject/server/exception/ExceptionCode.java
@@ -3,7 +3,11 @@ package com.preproject.server.exception;
 public enum ExceptionCode {
 
     MEMBER_ALREADY_EXISTS(400, "member_already_exists"),
-    MEMBER_NOT_EXISTS(404, "member_not_exists");
+    MEMBER_NOT_EXISTS(404, "member_not_exists"),
+    QUESTION_NOT_EXISTS(404, "question_not_exists"),
+    ANSWER_NOT_EXISTS(404, "answer_not_exists"),
+    ALREADY_LOGOUT_MEMBER(400, "already_logout_member");
+
     int code;
     String message;
     ExceptionCode(int code, String message) {

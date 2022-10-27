a081ffb3983908cb8f377093bff5fb7291056110
jongjeong <leem1315@gmail.com>
2022-10-27 17:33:36 +0900

    feat : 질문상세페이지 제작

--- client/src/pages/QuestionDetails.js
+++ client/src/pages/QuestionDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import RightSidebar from '../components/RightSidebar';
@@ -7,15 +7,14 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
 const Details = styled.div`
-  display: inline;
-  .title {
-    padding-top: 100px;
+  .leftSidebar {
+    position: fixed;
+    top: 0;
   }
-
-  // .left {
-  //   float: left;
-  // }///
-
+  .left {
+    margin-top: 55px;
+    position: fixed;
+  }
   .askbutton {
     background: #0a95ff;
     border: 1px solid #dbdee2;
@@ -25,31 +24,45 @@ const Details = styled.div`
     border-radius: 5px;
     font-size: 13;
   }
-`;
+  .contents {
+    margin-left: 180px;
+    display: flex;
+    align-items: center;
+    width: 760px;
+  }
+  .title {
+    margin: 0;
+    padding-top: 50px;
 
-const Right = styled.div`
-  padding-top: 100px;
-  padding-right: 50px;
+    font-size: 27px;
+  }
 `;
 
 const QuestionDetails = () => {
   return (
     <>
+      <Nav className="nav" />
       <Details>
-        <Nav />
-        <div>
-          <h1 className="title">title</h1>
-          <button className="askbutton" type="button">
-            Ask Question
-          </button>
+        <div className="left">
+          <Sidebar className="leftSidebar" />
         </div>
-        {/* <Sidebar className="left" /> */}
-        <Right>
-          <RightSidebar className="right" />
-        </Right>
+        <div className="contents">
+          <div className="title">
+            title, Using less (2.2.1) raises an error requiring therubyracer
+            cant install therubyracer on
+            windowsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </div>
+          <button className="askbutton">Ask Question</button>
+          <div>
+            <div>Asked</div>
+            <div>Modified </div>
+            <didv>Viewed </didv>
+          </div>
+        </div>
 
-        <Footer />
+        <RightSidebar />
       </Details>
+      <Footer />
     </>
   );
 };

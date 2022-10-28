268329b47ae86efebb252a3de078a14252456986
jongjeong <leem1315@gmail.com>
2022-10-28 14:08:01 +0900

    feat : 질문상세페이지 제작- 컬러 및 위치 수정

--- client/src/pages/QuestionDetails.js
+++ client/src/pages/QuestionDetails.js
@@ -7,10 +7,11 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
 const Details = styled.div`
-  padding-right: 24px;
+  padding-right: 165px;
+  padding-left: 150px;
   .leftSidebar {
     position: fixed;
-    top: 0;
+    // top: 0;
   }
   .left {
     margin-top: 55px;
@@ -27,8 +28,8 @@ const Details = styled.div`
   }
   .head {
     margin-left: 180px;
-    padding-top: 30px;
-    padding-left: 30px;
+    padding-top: 10px;
+    padding-left: 10px;
 
     display: flex;
     align-items: center;
@@ -47,6 +48,7 @@ const Details = styled.div`
     margin-right: 16px;
     font-size: 13px;
     margin-right: 2px;
+    margin-bottom: 20px;
     padding-bottom: 8px;
     border-bottom: 1px solid #eee;
     display: flex;
@@ -54,6 +56,7 @@ const Details = styled.div`
   .current div {
     margin-bottom: 8px;
     margin-right: 16px;
+    color: #babfc3;
   }
   .container {
     margin-left: 200px;
@@ -62,84 +65,121 @@ const Details = styled.div`
     display: flex;
   }
   //메인컨텐츠 내용
-  .contentsmain {
+  .contentspost {
     width: 100%;
     height: 100%;
   }
   .contents div {
     padding-right: 16px;
   }
+  svg {
+    fill: #babfc3;
+  }
+  .count {
+    color: #babfc3;
+    padding-left: 16px;
+  }
+  .vote {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .vote * {
+    margin-bottom: 10px;
+  }
+  .current .currentcontents {
+    color: black;
+    margin-left: 5px;
+  }
+  .contentmain {
+    width: 700px;
+  }
 `;
 
 const QuestionDetails = () => {
   return (
     <>
-      <Nav className="nav" />
+      <Nav />
       <Details>
         <div className="left">
           <Sidebar className="leftSidebar" />
         </div>
         <header className="head">
           <h1 className="title">
-            title, Using less (2.2.1) raises an error requiring therubyracer
-            cant install therubyracer on
+            title, 타이틀 제목 내용 Using less (2.2.1) raises an error requiring
+            therubyracer cant install therubyracer on
           </h1>
           <button className="askbutton">Ask Question</button>
         </header>
         <div className="container">
           <div className="current">
-            <div>Asked</div>
-            <div>Modified </div>
-            <didv>Viewed </didv>
+            <div>
+              Asked
+              <span className="currentcontents asked">today</span>
+            </div>
+            <div>
+              Modified
+              <span className="currentcontents Modified">today</span>
+            </div>
+            <div>
+              Viewed
+              <span className="currentcontents Viewed">1 times</span>
+            </div>
           </div>
           <div className="contents">
-            <div className="vote">vote</div>
-            {/* 위 세모 */}
-            <svg
-              // aria-hidden="true"
-              className="svg-icon iconArrowUpLg"
-              width="36"
-              height="36"
-              viewBox="0 0 36 36"
-            >
-              <path d="M2 25h32L18 9 2 25Z"></path>
-            </svg>
-            {/* 좋아요 갯수 */}
-            <div>0</div>
-            {/* 아래 세모 */}
-            <svg
-              aria-hidden="true"
-              className="svg-icon iconArrowDownLg"
-              width="36"
-              height="36"
-              viewBox="0 0 36 36"
-            >
-              <path d="M2 11h32L18 27 2 11Z"></path>
-            </svg>
-            <svg
-              aria-hidden="true"
-              className="js-saves-btn-unselected svg-icon iconBookmarkAlt"
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-            >
-              <path d="m9 10.6 4 2.66V3H5v10.26l4-2.66ZM3 17V3c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v14l-6-4-6 4Z"></path>
-            </svg>
-            <svg
-              aria-hidden="true"
-              className="mln2 mr0 svg-icon iconHistory"
-              width="19"
-              height="18"
-              viewBox="0 0 19 18"
-            >
-              <path d="M3 9a8 8 0 1 1 3.73 6.77L8.2 14.3A6 6 0 1 0 5 9l3.01-.01-4 4-4-4h3L3 9Zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5Z"></path>
-            </svg>
-            <div className="contentsmain">
-              메인내용 메인내용메인 메인내용 메인내용메인메인내용
-              메인내용메인메인내용 메인내용메인메인내용 메인내용메인메인내용
-              메인내용메인 메인내용 메인내용메인메인내용 메인내용메인메인내용
-              메인내용메인메인내용 메인내용메인 메인내용 메인내용메인
+            <div className="vote">
+              <svg
+                aria-hidden="true"
+                className="svg-icon iconArrowUpLg"
+                width="36"
+                height="36"
+                viewBox="0 0 36 36"
+              >
+                <path d="M2 25h32L18 9 2 25Z"></path>
+              </svg>
+              <div className="count">0</div>
+              <svg
+                aria-hidden="true"
+                className="iconArrowDownLg"
+                width="36"
+                height="36"
+                viewBox="0 0 36 36"
+              >
+                <path d="M2 11h32L18 27 2 11Z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                className="iconBookmarkAlt"
+                width="23"
+                height="23"
+                viewBox="0 0 18 18"
+                preserveAspectRatio="none"
+              >
+                <path d="m9 10.6 4 2.66V3H5v10.26l4-2.66ZM3 17V3c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v14l-6-4-6 4Z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                className="iconHistory"
+                width="23"
+                height="23"
+                viewBox="0 0 19 18"
+                preserveAspectRatio="none"
+              >
+                <path d="M3 9a8 8 0 1 1 3.73 6.77L8.2 14.3A6 6 0 1 0 5 9l3.01-.01-4 4-4-4h3L3 9Zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5Z"></path>
+              </svg>
             </div>
+            <div className="contentmain">
+              <div className="contentspost">
+                메인내용 Im getting the video Trailer data but it doesnt play
+                until I manually click the Play button (twice). But when I
+                switch to different routes and come back to the same, it plays
+                automatically. Again when I reload the page it doesnt.
+              </div>
+            </div>
+
+            {/* <div className="contentspost">
+              dddddddddddddddddddddddddddddddddddddd
+            </div> */}
             <RightSidebar />
           </div>
         </div>

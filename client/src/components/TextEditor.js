d03684772a6d22ff41bc155e9b1f9cf439ef20f0
yoosmg <sryu2s@gmail.com>
2022-10-28 11:42:22 +0900

    feat : 텍스트 에디터 입력값 관리 방식 변경

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -6,13 +6,21 @@ const TextEditor = forwardRef((props, ref) => {
   return (
     <>
       <Editor
-        initialValue="Please type here..." // 초기 입력값 설정
+        initialValue={props.value} // 초기 입력값
         previewStyle="tab" // vertical로 설정시 미리보기 화면 분할가능
         height="300px"
         initialEditType="markdown" // markdown or wysiwyg
         ref={ref}
-        onChange={props.onChange} // 부모 컴포넌트에서 ref, props 설정
-        // (e.g. <TextEditor ref={editorRef} onChange={getHTML}/>
+        onChange={props.onChange}
+        autofocus={false}
+        previewHighlight={false}
+        toolbarItems={[
+          ['heading', 'bold', 'italic', 'strike'],
+          ['hr', 'quote'],
+          ['ul', 'ol', 'task', 'indent', 'outdent'],
+          ['table', 'image', 'link'],
+          ['code', 'codeblock'],
+        ]}
       />
     </>
   );

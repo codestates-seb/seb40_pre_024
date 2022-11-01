8507c5478ec21ccd170ec4c9fd56d88d86f429b1
JKKIM <jkesonjk@gmail.com>
2022-11-01 14:40:37 +0900

    feat:메인페이지 데이터 통신 연결, 페이지네이션 추가 수정

--- client/src/components/QuestiosComponents/Pagination.js
+++ client/src/components/QuestiosComponents/Pagination.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { BsThreeDots } from 'react-icons/bs';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const Nav = styled.nav`
   display: flex;
@@ -58,7 +60,7 @@ export const sliceArrayBylimit = (total, limit) => {
     .fill()
     .map((_el, i) => i);
 
-  // 페이지를 pageLimite 단위로 끊어서 보여줄 배열의 수만큼 이중 배열 만들기
+  // 페이지를 pageLimit 단위로 끊어서 보여줄 배열의 수만큼 이중 배열 만들기
   return Array(Math.ceil(numPages / PAGE_LIMIT))
     .fill()
     .map(() => totalPageArray.splice(0, PAGE_LIMIT));
@@ -69,24 +71,26 @@ const Pagination = ({ total, limit, page, setPage }) => {
   const [totalPageArray, setTotalPageArray] = useState([]);
   // 필요한 페이지 수
   const numPages = Math.ceil(total / limit);
-  // console.log(totalPageArray);
-  // console.log(currentPageArray);
+  const location = useLocation();
+  // const navigate = useNavigate();
 
   useEffect(() => {
-    // 현재 페이지가
+    // 페이지 단위가 전환될 때
     if (page % PAGE_LIMIT === 1) {
       setCurrentPageArray(totalPageArray[Math.floor(page / PAGE_LIMIT)]);
+      // console.log('currentPageArray: ', currentPageArray);
     } else if (page % PAGE_LIMIT === 0) {
       setCurrentPageArray(totalPageArray[Math.floor(page / PAGE_LIMIT) - 1]);
     }
+    // getQuestions();
   }, [page]);
 
   useEffect(() => {
+    // 전체 데이터의 길이가 바뀔때
     const slicedPageArray = sliceArrayBylimit(total, limit);
-    // console.log(slicedPageArray); // [[0~14], [15~29], [30~44], [45~49]]
-    // console.log(slicedPageArray[numPages]); //undefined
     setTotalPageArray(slicedPageArray);
     setCurrentPageArray(slicedPageArray[0]);
+    // getQuestions();
   }, [total]);
 
   return (
@@ -96,15 +100,16 @@ const Pagination = ({ total, limit, page, setPage }) => {
           Prev
         </SideButton>
 
-        {currentPageArray.map((i) => (
-          <Button
-            key={i + 1}
-            onClick={() => setPage(i + 1)}
-            aria-current={page === i + 1 ? 'page' : null}
-          >
-            {i + 1}
-          </Button>
-        ))}
+        {currentPageArray && // 없으면 undefined 인 상태에서 map을 돌리기때문에 에러 발생
+          currentPageArray.map((i) => (
+            <Button
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              aria-current={page === i + 1 ? 'page' : null}
+            >
+              {i + 1}
+            </Button>
+          ))}
 
         {/* {page < 5 ? (
           currentPageArray.map((_el, i) => (

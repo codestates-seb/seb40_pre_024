69f8eca14fb609825cdbffdaa699feb187c2b566
billy5982 <kuu0404@naver.com>
2022-11-03 19:22:34 +0900

    fix : 제발 되주세용

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/api'],
+    '/api',
     createProxyMiddleware({
       target:
         'http://ec2-3-36-182-194.ap-northeast-2.compute.amazonaws.com:8080',
